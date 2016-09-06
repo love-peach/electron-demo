c64aeac416c65e3c1089e98fe105ccdcb356565b
love-peach <302784355@qq.com>
2016-09-06 18:53:29 +0800

    添加ignore

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # electron-demo
 
+## 版本对应关系
+
+electron|chromium
+--------|--------------
+0.37.6  |49.0.2623.75 
+0.37.2  |49.0.2623.75
+0.35.1  |45.0.2454.85
+
+
+
 ## 参考链接:
 
 [使用 Electron 构建桌面应用](https://zhuanlan.zhihu.com/p/20225295)
+[Electron 打包](https://segmentfault.com/a/1190000006210020)
--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-const electron = require('electron');
+//var electron = require('electron');
 // 控制应用生命周期的模块。
-const {app} = electron;
+//var app= electron.app;
 // 创建原生浏览器窗口的模块。
-const {BrowserWindow} = electron;
+//var BrowserWindow = electron.BrowserWindow;
 
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
 
 // 保持一个对于 window 对象的全局引用，如果你不这样做，
 // 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
-let mainWindow;
+var mainWindow = null;
 
-function createWindow() {
+// Electron 会在初始化后并准备
+// 创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
+app.on('ready', function createWindow() {
     // 创建浏览器窗口。
     mainWindow = new BrowserWindow({
         width: 1200,
@@ -17,11 +23,12 @@ function createWindow() {
     });
 
     // 加载应用的 index.html。
-    //mainWindow.loadURL(`file://${__dirname}/index.html`);
-    mainWindow.loadURL('http://mp.jiabangou.com');
+//    mainWindow.loadURL(`file://${__dirname}/index.html`);
+//    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('http://mp.test.xiaoyage.com');
 
     // 启用开发工具。
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
     // 当 window 被关闭，这个事件会被触发。
     mainWindow.on('closed', () => {
@@ -30,15 +37,10 @@ function createWindow() {
         // 与此同时，你应该删除相应的元素。
         mainWindow = null;
     });
-}
-
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数。
-// 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow);
+});
 
 // 当全部窗口关闭时退出。
-app.on('window-all-closed', () => {
+app.on('window-all-closed', function () {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
@@ -46,13 +48,13 @@ app.on('window-all-closed', () => {
     }
 });
 
-app.on('activate', () => {
-    // 在 macOS 上，当点击 dock 图标并且该应用没有打开的窗口时，
-    // 绝大部分应用会重新创建一个窗口。
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
+//app.on('activate', function () {
+//    // 在 macOS 上，当点击 dock 图标并且该应用没有打开的窗口时，
+//    // 绝大部分应用会重新创建一个窗口。
+//    if (mainWindow === null) {
+//        createWindow();
+//    }
+//});
 
 // 在这文件，你可以续写应用剩下主进程代码。
 // 也可以拆分成几个文件，然后用 require 导入。
